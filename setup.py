5cd7291581aeef346369c902fa451d30a11d3b58
David Carley <dacarley@gmail.com>
2021-03-06 01:26:14 -0800

    Added a filesystem watcher for uploaded files

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
         'scripts/edit-boot-config',
         'scripts/browser',
         ],
-    install_requires = 'tornado sockjs-tornado pyserial pyudev smbus2'.split(),
+    install_requires = 'tornado sockjs-tornado pyserial pyudev smbus2 watchdog'.split(),
     zip_safe = False,
     )
