9ef44446484976496a69410ab723488098dcc21f
David Carley <dacarley@gmail.com>
2021-03-06 09:16:48 -0800

    Allow rPi to use full screen resolution

--- scripts/install.sh
+++ scripts/install.sh
@@ -36,6 +36,15 @@ fi
 #./scripts/edit-boot-config gpu_mem=16
 #chmod ug+s /usr/lib/xorg/Xorg
 
+# Use the full screen resolution
+grep "^framebuffer_width=1280$" /boot/config.txt >/dev/null
+if [ $? -ne 0 ]; then
+    mount -o remount,rw /boot &&
+    sed -i 's/^\(framebuffer_.*\)$/#\1/g' /boot/config.txt
+    mount -o remount,ro /boot
+    REBOOT=true
+fi
+
 # Fix camera
 grep dwc_otg.fiq_fsm_mask /boot/cmdline.txt >/dev/null
 if [ $? -ne 0 ]; then
