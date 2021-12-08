1a2613677de2f175222996d4edf3553b21a99c06
David A. Carley <dacarley@gmail.com>
2021-12-08 10:04:36 -0800

    Update delete-cookies.py
    Neglected to commit the changes to the DB.

--- scripts/delete-cookies.py
+++ scripts/delete-cookies.py
@@ -4,3 +4,4 @@ import sqlite3
 db = sqlite3.connect('/home/pi/.config/chromium/Default/Cookies')
 cur = db.cursor()
 cur.execute("DELETE FROM cookies WHERE creation_utc < 13240000000000000")
+db.commit()
