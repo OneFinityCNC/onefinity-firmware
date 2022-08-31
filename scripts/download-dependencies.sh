0c58292347120166977e81e08e82a7578a699151
David Carley <dacarley@gmail.com>
2022-08-31 13:51:18 +0000

    Updated the python dependencies to include "evdev" for the latest gamepad support.

--- scripts/download-dependencies.sh
+++ scripts/download-dependencies.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
 
+# To use, copy this file, and requirements.txt into a folder on the controller, and run this script.
+# Then, copy the contents of ./installer/python-packages back to your development environment
+
 pip3 download -d installer/python-packages -r requirements.txt
