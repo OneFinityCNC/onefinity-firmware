5a35f3e241ca6e61092ec1d3f6ea5eaf93bb79f6
saifullah-N <n.mmsaifi16331@gmail.com>
2023-01-28 10:54:01 +0530

    auto select newly uploaded file test

--- src/js/gcode-viewer.js
+++ src/js/gcode-viewer.js
@@ -91,7 +91,7 @@ module.exports = {
         },
 
         reload: function(file) {
-            if (file == this.file) {
+            if (file != this.file) {
                 return;
             }
             console.log("reloaded :",file);
