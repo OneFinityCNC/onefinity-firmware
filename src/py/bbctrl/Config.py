4e75427eee8c396bbc25b94c1bf535fc10053157
saifullah-N <n.mmsaifi16331@gmail.com>
2023-03-19 22:23:36 +0530

    bumped version to 1.3.0-alpha1

--- src/py/bbctrl/Config.py
+++ src/py/bbctrl/Config.py
@@ -43,7 +43,7 @@ class Config(object):
         self.values = {}
 
         try:
-            self.version = "1.2.1"
+            self.version = "1.3.0-alpha1"
 
             # Load config template
             with open(get_resource('http/config-template.json'), 'r',
