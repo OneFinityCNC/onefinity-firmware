d8b8e40faea70476602d487ad668a998ca241b02
sanjayk03-dev <imsanjay003@gmail.com>
2024-01-12 03:25:57 +0530

    testing with logs

--- src/js/macros.js
+++ src/js/macros.js
@@ -122,6 +122,7 @@ module.exports = {
         this.state.selected_time;
       this.cancelMacros(this.tab - 1);
       this.confirmSave = false;
+      console.log(this.config.macros);
       try {
         await api.put("config/save", this.config);
         console.log("Successfully saved");
