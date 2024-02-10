29fa7dee2b76d4192a87b8d0c09becd085a4cfc4
sanjayk03-dev <imsanjay003@gmail.com>
2024-02-10 11:54:04 +0530

    logging svelte

--- src/svelte-components/src/lib/StoreHelpers.ts
+++ src/svelte-components/src/lib/StoreHelpers.ts
@@ -2,10 +2,11 @@ import { get, type Writable } from "svelte/store";
 
 export function waitForChange<T>(writable: Writable<T>): Promise<T> {
     let unsubscribe:()=>{};
-  const promise = new Promise(resolve => {
+  const promise = new Promise((resolve,reject) => {
     let receivedInitial = false;
     let previousValue : any;
     unsubscribe = writable.subscribe(value => {
+      console.log('StoreHelpers',{value:value,receivedInitial:receivedInitial,previousValue:previousValue})
       if (!receivedInitial) {
         receivedInitial = true;
         previousValue = value;
