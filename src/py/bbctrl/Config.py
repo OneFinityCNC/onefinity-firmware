fbed3549de0b1a7da3e00662c664c47149979a80
sanjayk03-dev <imsanjay003@gmail.com>
2024-04-06 15:19:49 +0530

    trying state update from config

--- src/py/bbctrl/Config.py
+++ src/py/bbctrl/Config.py
@@ -228,7 +228,10 @@ class Config(object):
             if not name in self.values: self.values[name] = {}
             self.values[name][index] = value
 
-        else: self.values[name] = value
+        else: 
+            self.values[name] = value
+            if name == "macros":
+                self.ctrl.state.config(name,value)
 
         # Update state variable
         if not 'code' in tmpl: return
