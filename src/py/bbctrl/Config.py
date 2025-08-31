28e6b29ed6e9c06b588500843482b5bac6ab7afb
sanjayk03-dev <imsanjay003@gmail.com>
2025-08-31 18:48:26 +0530

    v1.6.6

--- src/py/bbctrl/Config.py
+++ src/py/bbctrl/Config.py
@@ -43,7 +43,7 @@ class Config(object):
         self.values = {}
 
         try:
-            self.version = "1.6.5"
+            self.version = "1.6.6"
 
             # Load config template
             with open(get_resource('http/config-template.json'), 'r',
